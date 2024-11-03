15ac3bec903b9de02b6664161be426e01ee697c4
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-11-03 19:11:18 -0400

    swarm deletes original prefab

--- Base/Swarm.cs
+++ Base/Swarm.cs
@@ -39,6 +39,9 @@ public class Swarm : MonoBehaviour
 
         // Unrender self
         GetComponent<SpriteRenderer>().enabled = false;
+
+        // Remove prefab from scene
+        Destroy(prefab);
     }
 
     // Update is called once per frame
